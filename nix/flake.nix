f9980082e2395cec9a7d94bbfe352e32c0f75972
Martin Törnqvist <martin.toernqvist@gmail.com>
2024-09-14 20:22:55 +0200

    nix something

---
+++ nix/flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#ichigo'
+    nixosConfigurations.ichigo = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./nixos/configuration.nix ];
+    };
+  };
+}
