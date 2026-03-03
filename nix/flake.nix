bb14f2a70afd9141ca89880a5083699df89952be
Martin Törnqvist <maachin@gmail.com>
2026-03-03 23:01:17 +0100

    add public ssh key to nix config + hardening

--- nix/flake.nix
+++ nix/flake.nix
@@ -32,6 +32,9 @@
 		isNormalUser = true;
 		extraGroups = [ "input" "audio" "pipewire" "networkmanager" "wheel" ];
 		packages = with pkgs; [];
+		openssh.authorizedKeys.keys = [
+		  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOvmccLuoKuu0hxlj+sGean56+UzXx/cXwq3V14F89jh personal"
+		];
 	      };
 
               nixpkgs.config.allowUnfree = true;
