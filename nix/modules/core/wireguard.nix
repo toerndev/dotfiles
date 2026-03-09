87063d889e6ce61eca7df54b5c4c8350a32f6eb7
Martin Törnqvist <maachin@gmail.com>
2026-03-09 13:44:23 +0100

    add nixos wg mtu

--- nix/modules/core/wireguard.nix
+++ nix/modules/core/wireguard.nix
@@ -6,6 +6,7 @@
     ips = [ "10.100.0.1/24" ];
     listenPort = 51820;
     privateKeyFile = "/etc/secrets/wg-private-key";
+    mtu = 1280;
     peers = [
       { publicKey = "+9mageJp5UeDEDmj+EGw1inwy5BpMED0OjVu/tYF6U0="; allowedIPs = [ "10.100.0.2/32" ]; }  # Home laptop
       { publicKey = "k/A2D/QJE56P2gCF7iJi0LhOY8liZJ5jtorTiX/poB4="; allowedIPs = [ "10.100.0.3/32" ]; }  # Work laptop
