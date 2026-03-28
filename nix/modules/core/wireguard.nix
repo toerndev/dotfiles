10131472f9ef72b6090d12fb4e10120300799925
Martin Törnqvist <martin.toernqvist@gmail.com>
2026-03-28 20:47:53 +0100

    add phone to WG

--- nix/modules/core/wireguard.nix
+++ nix/modules/core/wireguard.nix
@@ -10,6 +10,7 @@
     peers = [
       { publicKey = "+9mageJp5UeDEDmj+EGw1inwy5BpMED0OjVu/tYF6U0="; allowedIPs = [ "10.100.0.2/32" ]; }  # Home laptop
       { publicKey = "k/A2D/QJE56P2gCF7iJi0LhOY8liZJ5jtorTiX/poB4="; allowedIPs = [ "10.100.0.3/32" ]; }  # Work laptop
+      { publicKey = "FawxhkgRS02QKDbbCmlJ+mZZLIsKiZwOz9mYXNmh3io="; allowedIPs = [ "10.100.0.4/32" ]; } # Android phone
     ];
   };
 
